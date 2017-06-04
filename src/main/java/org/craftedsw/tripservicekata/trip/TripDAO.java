5d9c3746bb66ea8bbd687bf51686c3a4e9d5f0c9
Juan Lladó <jllado@gmail.com>
2017-06-04 21:39:26 +0200

    Mark as deprecated the old method to find trips by user

--- src/main/java/org/craftedsw/tripservicekata/trip/TripDAO.java
+++ src/main/java/org/craftedsw/tripservicekata/trip/TripDAO.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public class TripDAO {
 
+	@Deprecated
 	public static List<Trip> findTripsByUser(User user) {
 		throw new CollaboratorCallException(
 				"TripDAO should not be invoked on an unit test.");
