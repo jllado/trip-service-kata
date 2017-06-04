8eead8b80e61d3101ed90608fa3265aea3c2554e
Juan Lladó <jllado@gmail.com>
2017-06-04 21:39:26 +0200

    Test TripService return zero trips when user is not friend of the logged in user

--- src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -7,13 +7,17 @@ import org.junit.runner.RunWith;
 import org.mockito.Spy;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.Mockito.doReturn;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TripServiceTest {
 
     private static final User NOT_LOGGED_IN_USER = null;
+    private static final User LOGGED_IN_USER = new User();
     private static final User ANY_USER = new User();
+    private static final User USER_WITHOUT_FRIENDS = new User();
 
     @Spy
     private TripService service = new TripService();
@@ -23,6 +27,13 @@ public class TripServiceTest {
         doReturn(NOT_LOGGED_IN_USER).when(service).getLoggedUserFromSession();
 
         service.getTripsByUser(ANY_USER);
+    }
+
+    @Test
+    public void return_zero_trips_when_user_is_not_friend_of_logged_in_user() throws Exception {
+        doReturn(LOGGED_IN_USER).when(service).getLoggedUserFromSession();
+
+        assertThat(service.getTripsByUser(USER_WITHOUT_FRIENDS), hasSize(0));
     }
 
 }
